949457456ce2c07d8e11a858f61e8139a44d2d43
danielpmc <danielpd93@gmail.com>
2020-11-10 03:01:52 +0000

    Start of the bot

---
+++ events/ready.js
@@ -0,0 +1,36 @@
+const exec = require('child_process').exec;
+module.exports = async (client, guild, files) => {
+
+    //Login message
+    console.log(chalk.magenta('[DISCORD] ') + chalk.green(client.user.username + " has logged in!"));
+
+    //Auto Activities List
+    const activities = [{
+            "text": "users manage their server",
+            "type": "WATCHING"
+        }
+    ];
+    setInterval(() => {
+        const activity = activities[Math.floor(Math.random() * activities.length)];
+        client.user.setActivity(activity.text, {
+            type: activity.type
+        });
+    }, 30000);
+
+    //Automatic 30second git pull.
+    setInterval(() => {
+        exec(`git pull`, (error, stdout) => {
+            let response = (error || stdout);
+            if (!error) {
+                if (response.includes("Already up to date.")) {
+                    //console.log('Bot already up to date. No changes since last pull')
+                } else {
+                    client.channels.get('766068015686483989').send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**")
+                    setTimeout(() => {
+                        process.exit();
+                    }, 1000)
+                };
+            }
+        })
+    }, 30000)
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+/*  ____              ____        __     __  __           __  _            
+   / __ \____ _____  / __ )____  / /_   / / / /___  _____/ /_(_)___  ____ _
+  / / / / __ `/ __ \/ __  / __ \/ __/  / /_/ / __ \/ ___/ __/ / __ \/ __ `/
+ / /_/ / /_/ / / / / /_/ / /_/ / /_   / __  / /_/ (__  ) /_/ / / / / /_/ / 
+/_____/\__,_/_/ /_/_____/\____/\__/  /_/ /_/\____/____/\__/_/_/ /_/\__, /  
+Free Hosting for ever!                                            /____/   
+*/
+
+//Package Imports
+const Discord = require("discord.js");
+global.client = new Discord.Client({ disableEveryone: true });
+const db = require("quick.db");
+const axios = require("axios");
+const fs = require("fs");
+global.chalk = require("chalk");
+
+var suckmynuts = true
+global.succ = suckmynuts;
+
+//Import config
+global.config = require('./config.json')
+
+//Create database for storing server settings
+global.settings = new db.table('settings');
+
+//Event handler
+fs.readdir('./events/', (err, files) => {
+    files = files.filter(f => f.endsWith('.js'));
+    files.forEach(f => {
+      const event = require(`./events/${f}`);
+      client.on(f.split('.')[0], event.bind(null, client));
+      delete require.cache[require.resolve(`./events/${f}`)];
+    });
+});
+
+//Bot login
+client.login(config.DiscordBot.Token);
